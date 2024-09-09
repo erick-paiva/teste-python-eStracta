289523c7b37a4171581ab8870daa77ed69e19fbd
Erick Paiva <erickpaivaasilva@gmail.com>
2024-09-09 17:11:35 -0400

    feat: make swagger url dynamic

--- app/configs/swagger.py
+++ app/configs/swagger.py
@@ -1,10 +1,14 @@
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask import request
 
 
-def init_app(app):
-    SWAGGER_URL = "/documentation"
-    API_URL = "/static/swagger.json"
-    swaggerui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL, API_URL, config={"app_name": "Company API"}
-    )
-    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+def init_swagger(app):
+    @app.before_request
+    def adjust_swagger_url():
+        SWAGGER_URL = "/swagger"
+        API_URL = f"{request.host_url.rstrip('/')}/static/swagger.json"
+
+        swaggerui_blueprint = get_swaggerui_blueprint(
+            SWAGGER_URL, API_URL, config={"app_name": "Company API"}
+        )
+        app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
