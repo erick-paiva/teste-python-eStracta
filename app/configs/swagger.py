924678594ac7ec0a8a681cc9da7ed11411c25802
Erick Paiva <erickpaivaasilva@gmail.com>
2024-09-09 17:39:02 -0400

    feat: update swagger host

--- app/configs/swagger.py
+++ app/configs/swagger.py
@@ -1,19 +1,10 @@
-from flask import request
 from flask_swagger_ui import get_swaggerui_blueprint
 
 
 def init_app(app):
-    SWAGGER_URL = "/swagger"
-
-    @app.context_processor
-    def override_url():
-        API_URL = request.host_url.rstrip("/") + "/static/swagger.json"
-        swaggerui_blueprint = get_swaggerui_blueprint(
-            SWAGGER_URL, API_URL, config={"app_name": "Company API"}
-        )
-        return dict(swagger_ui_blueprint=swaggerui_blueprint)
-
+    SWAGGER_URL = "/documentation"
+    API_URL = "/static/swagger.json"
     swaggerui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL, "/static/swagger.json", config={"app_name": "Company API"}
+        SWAGGER_URL, API_URL, config={"app_name": "Company API"}
     )
     app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
