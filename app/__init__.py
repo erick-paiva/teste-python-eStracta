a8ea660f01b4d82207f667f990027d915986e3ad
Erick Paiva <erickpaivaasilva@gmail.com>
2024-09-09 16:47:46 -0400

    feat: add flask cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from app.configs import migration, database, swagger, config
 from app import routes
 from app.middlewares.auth_middleware import add_bearer_token
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
@@ -16,3 +16,5 @@ migration.init_app(app)
 swagger.init_app(app)
 
 app.before_request(add_bearer_token)
+
+CORS(app)
