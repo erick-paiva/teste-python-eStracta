f445d7374ab6ebc10c293fe93979130712c68b45
Erick Paiva <erickpaivaasilva@gmail.com>
2024-09-09 15:30:19 -0400

    feat: add database url

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,15 @@ from app import routes
 from app.middlewares.auth_middleware import add_bearer_token
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(config.Config)
+app = Flask(__name__)
+app.config.from_object(config.Config)
 
-    migration.init_app(app)
+routes.init_app(app)
 
-    database.init_app(app)
+migration.init_app(app)
 
-    routes.init_app(app)
+database.init_app(app)
 
-    swagger.init_app(app)
+swagger.init_app(app)
 
-    app.before_request(add_bearer_token)
-
-    return app
+app.before_request(add_bearer_token)
