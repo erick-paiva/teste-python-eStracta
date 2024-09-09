44a839ace4acfeced82d47b2340d76b0f8c9d6f9
Erick Paiva <erickpaivaasilva@gmail.com>
2024-09-09 16:42:33 -0400

    refactor: change database

--- app/configs/config.py
+++ app/configs/config.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = "asdasdasd"
-    SQLALCHEMY_DATABASE_URI = "postgres://default:wq5fBl0cUekI@ep-young-snow-a4bfflwe-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.getenv("FLASK_ENV") == "development"
