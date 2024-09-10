9914ae7b38174281968e628e60883e2fb734548b
Erick Paiva <erickpaivaasilva@gmail.com>
2024-09-10 00:08:30 -0400

    feat: resolve error in cors

--- app/routes/company_routes.py
+++ app/routes/company_routes.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, request
 from app.controllers.company_controller import CompanyController
 from app.controllers.auth_controller import AuthController
+from flask_cors import cross_origin
 
 company_bp = Blueprint("company_bp", __name__)
 
 
 @company_bp.route("/login", methods=["POST"])
+@cross_origin()
 def login():
     return AuthController.login()
 
 
 @company_bp.route("/", methods=["POST"])
+@cross_origin()
 @AuthController.token_required
 def create_company():
     data = request.json
@@ -18,6 +21,7 @@ def create_company():
 
 
 @company_bp.route("/<cnpj>", methods=["PUT"])
+@cross_origin()
 @AuthController.token_required
 def update_company(cnpj):
     data = request.json
@@ -25,12 +29,14 @@ def update_company(cnpj):
 
 
 @company_bp.route("/<cnpj>", methods=["DELETE"])
+@cross_origin()
 @AuthController.token_required
 def delete_company(cnpj):
     return CompanyController.delete_company(cnpj)
 
 
 @company_bp.route("/", methods=["GET"])
+@cross_origin()
 @AuthController.token_required
 def list_companies():
     params = request.args
